4aa6a707851670d493070a54ca1925b9be498288
Jullan Quevedo <jullan.quevedo50@gmail.com>
2020-11-23 22:10:36 +1300

    new

--- middleware/api.js
+++ middleware/api.js
@@ -1,10 +1,11 @@
 const express           = require('express')
-const router            = express.Router()
+const app            = express.Router()
 const sgMail = require('@sendgrid/mail')
 const axiosjs           = require('axios')
 const https             = require('https')
 const { body }          = require('express-validator')
 const { sanitizeBody }  = require('express-validator')
+const nodemailer = require('nodemailer')
 
 const axios = axiosjs.create({
   headers: {
@@ -12,44 +13,52 @@ const axios = axiosjs.create({
     'Access-Control-Allow-Origin': '*'
   }
 })
+app.post('/send-email', [
+  body('*')
+  .not().isEmpty()
+  .trim()
+  .escape(),
+], (req, res) => {
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  console.log('Email Details:', JSON.stringify(req.body, 0, 2))
 
-router.post('/send-email',function(req, res){
-const msg = {
-  to: 'jullan.quevedo50@gmail.com', // Change to your recipient
-  from: req.body.email, // Change to your verified sender
-  subject: req.body.subject,
-  text: req.body.message + "\n\nRegards,\n " + req.body.name,
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+  var transport = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 25,
+    auth: {
+      user: "apikey",
+      pass: "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"
+    }
+  });
+
+  var mailOptions = {
+    from: 'jullan.quevedo50@gmail.com',
+    to: 'jullan.quevedo50@gmail.com',
+    subject: 'Inquiry Received From: '+ req.body.email + '  ' +req.body.subject,
+    text: req.body.message + "\n\nRegards,\n " + req.body.name +'\n'
+  };
+
+  transport.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log('ERROR!!! '+ JSON.stringify(error))
+      return res.status(400).json({
+        success: false,
+        message: 'Opps It seems something went wrong. Please try again'
+      })
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: 'Thanks for contacting Clearvision. We\'ll be in contact soon'
+      })
+    }
+  });
+
 })
-router.get('/welcome', (req, res) => {
+
+app.get('/test', (req, res) => {
     return res.status(200).json({
       success: true,
       message: 'Welcome to my landing page backend'
     })
   })
-
-router.post('/send-email',function(req, res){
-  response = {
-    name: req.body.name,
-    email: req.body.email,
-    subject: req.body.subject,
-    message: req.body.message
-  }
-
-  return res.status(200).json({
-    success: true,
-    message: 'Email saved'
-  })
-})
-
-module.exports = router
+module.exports = app
